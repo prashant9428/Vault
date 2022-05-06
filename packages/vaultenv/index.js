b82eba99d63414ba20312893c01d2bfcf993056c
prashant9428 <pathakprashant841@gmail.com>
2022-05-06 10:11:10 +0530

    fix: updated the production pipeline

--- packages/vaultenv/index.js
+++ packages/vaultenv/index.js
@@ -10,6 +10,8 @@ function config({ dotenvPath = "" } = {}) {
 
         
 
+
+        
         
         const jsonFromEnv = getParsedEnv(dotenvPath);
         const { isKeyFound, notFoundAttr } = isRequiredKeyPresent(jsonFromEnv);
