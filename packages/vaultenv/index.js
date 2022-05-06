5434eccd932fa97732d932600e41799fe348f11b
prashant9428 <pathakprashant841@gmail.com>
2022-05-06 19:18:23 +0530

    fix: added a release notes

--- packages/vaultenv/index.js
+++ packages/vaultenv/index.js
@@ -10,12 +10,6 @@ function config({ dotenvPath = "" } = {}) {
 
 
 
-
-        
-
-
-
-
         
         const jsonFromEnv = getParsedEnv(dotenvPath);
         const { isKeyFound, notFoundAttr } = isRequiredKeyPresent(jsonFromEnv);
