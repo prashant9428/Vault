4537a009fdc3320a80a8fd0a0044477d51ea4458
prashant9428 <pathakprashant841@gmail.com>
2022-05-19 09:58:11 +0530

    fix: changed auto config

--- auto.config.js
+++ auto.config.js
@@ -1,5 +1,5 @@
-module.exports={
-  "prereleaseBranches": ["development"],
+module.exports = {
+  
   "changelog": {  
     "message": "string"
   },
@@ -10,6 +10,8 @@ module.exports={
     "onlyGraduateWithReleaseLabel": true
   },
   "release":{
-    "prerelease": true
+    "prereleaseBranches": ["development"],
+    "prerelease": true,
+    "base-branch": "main"
   }
 }
